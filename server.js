435df18ebc067481a883f473defb4a2b2d7960c7
Aniruddha Tapas <aniruddhastapas@gmail.com>
2018-08-09 20:41:22 -0400

    Update CORS

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200", "https://cs5610-whiteboard-client-ng.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "https://cs5610-whiteboard-client-ng.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
